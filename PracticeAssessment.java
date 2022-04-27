47286c58172fbd00695390c30db4f0a1eab9bb6f
NIPHAM53 <nipham53@gmail.com>
2022-04-27 17:57:46 -0400

    First Commit

---
+++ PracticeAssessment.java
@@ -0,0 +1,38 @@
+package co.grandcircus;
+
+public class PracticeAssessment {
+
+	public static void main(String[] args) {
+
+	}
+public static void espanol() {
+	//return map with integer key:1,2,3 and the corresponding String value
+	//"Uno:, "Dos", "Tres".
+}
+public static int lift(String a) {
+	// Takes list of strings as a parameter 
+	//and modifies it to turn all caps. 
+	//Returns the length of the list
+	// {"Hi", "mom"!}>{"HI","MOM!"} and returns 2
+	
+	return 0;
+	
+}
+public static boolean takeItAway(String b) {
+	//parameters are a map with string keys and values and 
+	//a string
+	//modify the map parameters by removing the key that matches the second
+	//parameters. Return true if the key had been in the original map.
+	//return false if not. 
+	return false;
+}
+public static void prependValue() {
+	
+}
+public static void listToSet() {
+	
+}
+public static int catchThis(int c) {
+	return 0;
+}
+}
